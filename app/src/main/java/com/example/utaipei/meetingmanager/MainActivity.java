338d45be7264b724c3b211a03facbd629f2ecdad
HsinYa <starrysky012499@gmail.com>
2017-07-11 23:14:18 +0800

    create new activity named MeetingList

--- app/src/main/java/com/example/utaipei/meetingmanager/MainActivity.java
+++ app/src/main/java/com/example/utaipei/meetingmanager/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.utaipei.meetingmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button)findViewById(R.id.signIn);
+        login.setOnClickListener(loginListener);
     }
+
+    private Button.OnClickListener loginListener = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this,MeetingList.class);
+            startActivity(intent);
+        }
+    };
+
 }
---
+++ app/src/main/java/com/example/utaipei/meetingmanager/MeetingList.java
@@ -0,0 +1,17 @@
+package com.example.utaipei.meetingmanager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cindy on 2017/7/11.
+ */
+
+public class MeetingList extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.meeting_list);
+    }
+}
