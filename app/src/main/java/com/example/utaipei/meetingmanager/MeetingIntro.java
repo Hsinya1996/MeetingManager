4022154d203dc712f4d3c65e83674596fec73c1a
Hsinya1996 <u10316005@go.utaipei.edu.tw>
2017-07-16 14:23:04 +0800

    interface about meeting information

--- app/src/main/java/com/example/utaipei/meetingmanager/MeetingIntro.java
+++ app/src/main/java/com/example/utaipei/meetingmanager/MeetingIntro.java
@@ -2,6 +2,7 @@ package com.example.utaipei.meetingmanager;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +16,9 @@ public class MeetingIntro extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.meeting_intro, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.tv);
-        textView.setText("intro");
+        TextView textView = (TextView) rootView.findViewById(R.id.content);
+        textView.setText("rtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgjrtchuheolifhrioejwijform;irjolijflkje;orijfirjflkrjmfojrijrhvioljvim;jrjt;ijvlhlihgj;boj/;rlk;ljkbjkgjkrhljhljrhlulwknvoknb;kmrkmglk");
+        textView.setMovementMethod(ScrollingMovementMethod.getInstance());
         return rootView;
     }
 }
