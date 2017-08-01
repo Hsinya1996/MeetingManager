6e7807a146b482391108eea451e69db9065a7fa5
Hsinya1996 <u10316005@go.utaipei.edu.tw>
2017-08-01 19:05:49 +0800

    Modify the interface of MeetingList

--- app/src/main/java/com/example/utaipei/meetingmanager/MeetingList.java
+++ app/src/main/java/com/example/utaipei/meetingmanager/MeetingList.java
@@ -1,13 +1,18 @@
 package com.example.utaipei.meetingmanager;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -20,8 +25,9 @@ import java.util.HashMap;
  */
 
 public class MeetingList extends AppCompatActivity{
-    public TextView tv;
-    private ListView listView;
+    private TextView tv,lname,ltime,lplace;
+    private Button sign0ut,lcheck;
+    //private ListView listView;
 
 
     @Override
@@ -36,7 +42,51 @@ public class MeetingList extends AppCompatActivity{
         tv.setText(content);
 
         //meeting list
-        listView = (ListView) findViewById(R.id.list);
+        Context mContext = MeetingList.this;
+        // 取得 LinearLayout 物件
+        LinearLayout ll = (LinearLayout) findViewById(R.id.lists);
+        // 將feedviews加入到 LinearLayout 中
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
+        String[] titles = new String[]{ "預算審查" , "報表分析", "部門會議","預算審查" , "報表分析", "部門會議","預算審查" , "報表分析", "部門會議","報表分析", "部門會議" };
+        String[] places = new String[]{ "G509" , "C313", "H412" ,"G509" , "C313", "H412" ,"G509" , "C313", "H412","C313", "H412" };
+        String[] times = new String[]{ "9-11" , "13-16", "15-18" ,"9-11" , "13-16", "15-18" ,"9-11" , "13-16", "15-18","13-16", "15-18" };
+        int[] a = new int[]{1,0,1,1,0,1,1,1,0,1,0};
+
+        for( int i=0;i<titles.length ; ++i) {
+            final View view = inflater.inflate(R.layout.lvitem, null, true);
+            lname = (TextView)view.findViewById(R.id.name);
+            ltime =  (TextView)view.findViewById(R.id.time);
+            lplace = (TextView)view.findViewById(R.id.place);
+            lcheck = (Button)view.findViewById(R.id.check);
+            lname.setText(titles[i]);
+            ltime.setText(times[i]);
+            lplace.setText(places[i]);
+            if(a[i]==0){
+                lcheck.setVisibility(View.INVISIBLE);
+            }
+            ll.addView(view);
+            lcheck.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+
+                }
+            });
+            lcheck.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    Intent intent = new Intent();
+                    intent.setClass(MeetingList.this,Meeting.class);
+                    startActivity(intent);
+                }
+            });
+        }
+        sign0ut = (Button)findViewById(R.id.signout);
+        sign0ut.setOnClickListener(signoutListener);
+        //ll.addView(view,0);
+
+        /*listView = (ListView) findViewById(R.id.list);
         // 清單陣列
         ArrayList<HashMap<String,String>> data = new ArrayList<HashMap<String,String>>();
         String[] titles = new String[]{ "預算審查" , "報表分析", "部門會議","預算審查" , "報表分析", "部門會議","預算審查" , "報表分析", "部門會議" };
@@ -60,7 +110,28 @@ public class MeetingList extends AppCompatActivity{
                 intent.setClass(MeetingList.this,Meeting.class);
                 startActivity(intent);
             }
-        });
+        });*/
 
+    }
+
+    private Button.OnClickListener signoutListener = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setClass(MeetingList.this,MainActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) { // 监控/拦截/屏蔽返回键
+                return false;
+        } else if (keyCode == KeyEvent.KEYCODE_MENU) {
+                //do something
+        } else if (keyCode == KeyEvent.KEYCODE_HOME) {
+                //这里操作是没有返回结果的
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
