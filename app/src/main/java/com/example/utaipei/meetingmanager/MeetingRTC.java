c53916088754b643dcfa8ef1e54cee749d44dd55
楊恭寧 <e04su307@gmail.com>
2017-08-30 17:23:51 +0800

    apprtc20170830
    20170830

--- app/src/main/java/com/example/utaipei/meetingmanager/MeetingRTC.java
+++ app/src/main/java/com/example/utaipei/meetingmanager/MeetingRTC.java
@@ -5,7 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.content.Intent;
+import android.view.View.OnClickListener;
 
 /**
  * Created by cindy on 2017/7/16.
@@ -15,8 +18,15 @@ public class MeetingRTC extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.meeting_rtc, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.tv);
-        textView.setText("rtc");
+        Button button = (Button) rootView.findViewById(R.id.rtc);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), ConnectActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return rootView;
     }
 }
